c626910f77cbcef6aa94bbe5f08cce1aa23af383
Ktsirakos <tkonstantinos1996@hotmail.com>
2017-12-14 14:19:18 +0200

    test it

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define START_PROGRAMM int main(){
+#define END_PROGRAMM   return 0; }
+
+
+START_PROGRAMM
+    cout << "Hello World!" << endl;
+END_PROGRAMM
+
+
+//just an addition
