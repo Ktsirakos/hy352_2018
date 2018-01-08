7a3f985d9e124e716d38c43c8ae7d054d809affd
Unknown <thanosbatzi@gmail.com>
2018-01-08 18:01:40 +0200

    commit if for

--- hy352_files/LOGO.h
+++ hy352_files/LOGO.h
@@ -14,6 +14,154 @@
 #define TRUE true 
 #define FALSE false
 
+#define IF ;if(
+#define AND and_comparer
+#define OR or_comparer
+#define NOT not_comparer
+#define ELSE ;}else{
+#define ELIF ;}else if(
+#define DO ){
+#define END ;}
+
+
+#define SUM add
+#define DIFFERENCE sub
+#define MINUS min
+#define PRODUCT mul
+#define QUOTIENT divi
+#define MODULO mod
+
+
+#define REPEAT ;for(int i_ = 2 ; i_<
+#define WHILE i_;){} int i_=1;while(i_++ &&
+#define TIMES +2;i_++
+#define REPCOUNT  i_-1
+
+
+
+
+
+
+
+
+
 #define LIST \
 
- 
+
+template<typename T>
+bool and_comparer(T a) {
+    // In real-world code, we wouldn't compare floating point values like
+    // this. It would make sense to specialize this function for floating
+    // point types to use approximate comparison.
+    return a == true;
+}
+
+template<typename T, typename... Args>
+bool and_comparer(T a, Args... args) {
+    return a ==true && and_comparer(args...);
+}
+
+
+
+template<typename T>
+bool or_comparer(T a) {
+    // In real-world code, we wouldn't compare floating point values like
+    // this. It would make sense to specialize this function for floating
+    // point types to use approximate comparison.
+    return a ==true;
+}
+
+template<typename T, typename... Args>
+bool or_comparer(T a, Args... args) {
+    return a == true || or_comparer(args...);
+}
+
+
+template<typename T>
+bool not_comparer(T a) {
+    // In real-world code, we wouldn't compare floating point values like
+    // this. It would make sense to specialize this function for floating
+    // point types to use approximate comparison.
+    return a ==false;
+}
+
+template<typename T, typename... Args>
+bool not_comparer(T a, Args... args) {
+    return a == false && not_comparer(args...);
+}
+
+
+
+template<typename T>
+T add(T v) {
+    return v;
+}
+
+template<typename T, typename... Args>
+T add(T first, Args... args) {
+    return first + add(args...);
+}
+
+
+template<typename T>
+T sub(T v) {
+    return v;
+}
+
+template<typename T, typename... Args>
+T sub(T first, Args... args) {
+    return first - sub(args...);
+}
+
+
+template<typename T>
+T mul(T v) {
+    return v;
+}
+
+template<typename T, typename... Args>
+T mul(T first, Args... args) {
+    return first * mul(args...);
+}
+
+
+//template<typename T>
+//T div(T v) {
+//    return v;
+//}
+//
+//template<typename T, typename... Args>
+//T div(T first, Args... args) {
+//    return first / div(args...);
+//}
+//
+//template<typename T>
+//T mod(T v) {
+//    return v;
+//}
+//
+//template<typename T, typename... Args>
+//T mod(T first, Args... args) {
+//    return first % mod(args...);
+//}
+
+
+int divi(int x,int y){
+    int k=x/y;
+    return k;
+
+}
+
+int mod(int x,int y){
+    int k=x%y;
+    return k;
+    
+}
+
+
+int min(int x){
+    int k=-x;
+    //std::cout << k << std::endl;
+    return k;
+}
+
--- hy352_files/hy352_files/main.cpp
+++ hy352_files/hy352_files/main.cpp
@@ -16,10 +16,104 @@ START_PROGRAMM
     MAKE a = WORD: "come with me"
     MAKE boolean = BOOLEAN: FALSE
     MAKE integer = NUMBER:20
+
     PRINT(a)
     PRINT(hello)
     PRINT(boolean)
-    PRINT(integer)
+PRINT(integer);
+int x=10;
+int y=30;
+int k= SUM(NUMBER:1, 2, 3,PRODUCT (2,2),20,MODULO (12, 12));
+PRINT(k)
+
+
+MAKE t = DIFFERENCE(NUMBER:10,10,-10,20,-integer,MODULO (12, 12))
+PRINT(t)
+//t=hello;
+//PRINT(t);
+MAKE n = PRODUCT (2,2,MODULO (12, 12))
+PRINT(n)
+
+MAKE v5 = MODULO (14, 12)
+PRINT(v5)
+
+MAKE v4 = QUOTIENT (NUMBER: 24, PRODUCT (2,6))
+PRINT(v4)
+
+
+MAKE v2 = MINUS (12)
+PRINT(v2)
+
+
+
+
+IF AND(x==10,integer==20,y==30) DO
+   // IF AND(x==10,integer==20) DO
+    PRINT(x)
+   // END
+END
+
+IF OR(x>10,integer==10) DO
+PRINT(x)
+ELIF x==(NUMBER:10) DO
+PRINT(boolean)
+END
+
+int w=31;
+IF NOT (w==30,x==10) DO
+PRINT(w)
+END
+
+//for(int i=0;i<6;i++){
+//    w++;
+//}
+//PRINT(w);
+REPEAT 5 TIMES DO
+REPCOUNT
+PRINT(REPCOUNT);
+w++
+END
+PRINT(w);
+
+
+
+int r=13;
+//for(int i=0;r!=25;){
+//    r++;
+//    for(int i=0;r!=25;){
+//    }
+//
+//}
+//PRINT(r);
+REPEAT WHILE r!=17 DO
+REPCOUNT
+PRINT(REPCOUNT);
+ r++;
+END
+PRINT(r);
+
+//int z=20;
+//REPEAT  WHILE z!=26 DO
+//REPCOUNT
+//PRINT(REPCOUNT);
+//z++;
+//END
+//PRINT(z);
+//int z=20;
+//int c=1;
+////PRINT(c);
+//while(c++ && z!=26){
+//    int g=c-1;
+//    PRINT(g);
+//    //PRINT(c);
+////    REPCOUNT
+////    PRINT(REPCOUNT);
+//    z++;
+//
+//}
+//PRINT(z);
+
+
 
 END_PROGRAMM
 
